131c1162125bd61e6bd670e247fd2841fc1bd226
rokimaru <rokimaru@users.noreply.github.com>
2020-10-19 10:17:42 +0300

    soap_url вынесен в конфигурационный файл

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,7 +9,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client('http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php?wsdl')
+        client = Client(self.app.soap_url)
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,9 +17,10 @@ class SoapHelper:
             return False
 
     def get_project_list(self):
-        client = Client('http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php?wsdl')
+        username = self.app.config['webadmin']['username']
+        password = self.app.config['webadmin']['password']
+        client = Client(self.app.soap_url)
         project_list = []
-        for project in client.service.mc_projects_get_user_accessible(self.app.config['webadmin']['username'],
-                                                                      self.app.config['webadmin']['password']):
+        for project in client.service.mc_projects_get_user_accessible(username, password):
             project_list.append(Project(name=project.name, id=project.id))
         return project_list
